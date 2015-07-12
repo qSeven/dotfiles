2a0d989507a812481b512e64df1d07e82316001e
qSeven <shane.p.kelly7@gmail.com>
2015-07-12 00:28:23 -0700

    fixed git confusion

---
+++ MyAlias.sh
@@ -0,0 +1,26 @@
+alias untar='tar xvzf'
+
+Xise(){
+	if [ -f /opt/Xilinx/14.2/ISE_DS/settings64.sh ]
+	then
+		source /opt/Xilinx/14.2/ISE_DS/settings64.sh >/dev/null
+	fi
+	ise &
+}
+clear
+
+
+# generate shortened pwd entry similar to csh's, for the PS1 prompt
+function shorten {
+	PS1='${PWD##*/}$ '
+}
+
+#function rmUSBsafe() {
+#       if [ $# -eq 0 ]
+#       then
+#       	echo "No arguments supplied"
+#       else
+#
+#       fi
+#{
+alias memUse='ps -eo rss,vsz,pid,cputime,cmd --width 100 --sort rss,vsz | tail --lines 10'
